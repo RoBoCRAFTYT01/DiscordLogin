20ffbda0968476e53e42497846e1a9de2ae88b5b
RoBoCRAFTYT01 <fa83734@gmail.com>
2025-02-25 20:17:43 +0100

    v0.0.1

--- src/server.js
+++ src/server.js
@@ -3,32 +3,43 @@ const session = require("express-session");
 const passport = require("passport");
 const DiscordStrategy = require("passport-discord").Strategy;
 const path = require("path");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
 
+dotenv.config();
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "../public"))); 
+app.use(express.static(path.join(__dirname, "../public")));
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB Connection Error:", err));
 
 app.use(
     session({
-        secret: "dqsdqsDSQJfof546fqDd6846#àdsq@dksqIoùdsq21641531dqs4dQSD4dS#.DSQ5@1",
+        secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI,
+            collectionName: "sessions",
+            ttl: 14 * 24 * 60 * 60,
+        }),
     })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((obj, done) => {
-    done(null, obj);
-});
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
 
 passport.use(
     new DiscordStrategy(
